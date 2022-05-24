71d28b148a507857ed787c8b0d25d3984e414aa6
disam8853 <wang.sam33@gmail.com>
2022-05-24 21:53:23 +0800

    update gs

--- global-server/main.py
+++ global-server/main.py
@@ -1,15 +1,45 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, Response
 from environs import Env
+import aiohttp
+import asyncio
 
 env = Env()
 env.read_env()
 
 app = Flask(__name__)
+collaborators = env("COLLABORATORS_URL").split(',')
+print(collaborators)
 
 
 @app.route("/", methods=['GET'])
 def get():
     return 'OK'
+
+
+############# model #################
+async def get(url, session):
+    try:
+        async with session.get(url=url) as response:
+            resp = await response.read()
+            return resp
+    except Exception as e:
+        print(f"Unable to get url {url} due to {e.__class__}.")
+        raise e
+
+
+@app.route("/model/train", methods=["GET"])
+async def train_model():
+    try:
+        async with aiohttp.ClientSession() as session:
+            await asyncio.gather(*[get(f'{url}/data/process', session) for url in collaborators])
+    except Exception:
+        return Response('Request a train failed', 500)
+    print("All collaborators have been noticed")
+    return 'ok'
+
+@app.route("/model/data", methods=["POST"])
+def receive_data():
+    return 'ok'
 
 @app.route("/clip", methods=['POST'])
 def callback():
