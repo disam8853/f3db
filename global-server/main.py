b2229d300f1bc93d266585898c32289deef64004
disam8853 <wang.sam33@gmail.com>
2022-05-28 19:50:19 +0800

    update docker compose and add POST /pipeline

--- global-server/main.py
+++ global-server/main.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, abort, Response
+from flask import Flask, request, abort, Response, jsonify, json
 from environs import Env
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 import aiohttp
 import asyncio
+import json
 
 env = Env()
 env.read_env()
@@ -9,6 +12,9 @@ env.read_env()
 app = Flask(__name__)
 collaborators = env("COLLABORATORS_URL").split(',')
 print(collaborators)
+
+db_client = MongoClient(env("MONGODB_URL"))
+pipelines_db = db_client['f3db'].pipelines
 
 
 @app.route("/", methods=['GET'])
@@ -19,9 +25,17 @@ def get():
 ############# model #################
 async def post(url, data, session):
     try:
-        async with session.post(url=url, data=data) as response:
-            resp = await response.read()
-            return resp
+        async with session.post(url=url, json=data) as response:
+            if response.status == 200:
+                print("Status:", response.status)
+                print("Content-type:", response.headers['content-type'])
+                return await response.json()
+            else:
+                print(response.status)
+                raise Exception('connection failed!')
+    except aiohttp.ClientConnectorError as e:
+        print('Connection Error', str(e))
+        raise e
     except Exception as e:
         print(f"Unable to get url {url} due to {e.__class__}.")
         raise e
@@ -37,21 +51,26 @@ async def train_model():
     print("All collaborators have been noticed")
     return 'ok'
 
+
 @app.route("/model/data", methods=["POST"])
 def receive_data():
     return 'ok'
 
-@app.route("/clip", methods=['POST'])
-def callback():
-
-    body = request.json
-    txt = body['text']
-    max_img_cnt = body['max_img_cnt']
-
-    return {"max_img_cnt": max_img_cnt}
+############ Pipeline ############
 
 
-if __name__ == "__main__":
-    app.run(host=env("flask_host"), port=env("flask_port"))
-    # from waitress import serve
-    # serve(app, host=config['ENV']['HOST'], port=config['ENV']['PORT']) 
+@app.route('/pipeline', methods=['POST'])
+async def create_pipeline():
+    try:
+        async with aiohttp.ClientSession() as session:
+            res = await asyncio.gather(*[post(f'{url}/pipeline', {"collaborator": request.json['collaborator']}, session) for url in collaborators])
+    except Exception as e:
+        return Response('Request to create pipeline failed!' + str(e), 500)
+    print("All collaborators have created pipeline")
+
+    try:
+        pipeline_id = pipelines_db.insert_one(request.json).inserted_id
+    except Exception:
+        return Response('Failed to insert into db!', 500)
+
+    return jsonify({"id": str(pipeline_id), "collaborator": res})
