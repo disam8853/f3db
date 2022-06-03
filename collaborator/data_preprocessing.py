4b02da7d2dd363b7f894ea0c4ed751ad62a24937
disam8853 <wang.sam33@gmail.com>
2022-06-03 19:19:44 +0800

    update

--- collaborator/data_preprocessing.py
+++ collaborator/data_preprocessing.py
@@ -13,14 +13,15 @@ from sklearn.model_selection import train_test_split
 env = Env()
 env.read_env()
 DATA_FOLDER = env('DATA_FOLDER')
-GLOBAL_SERVER_URL=env('GLOBAL_SERVER_URL')
+GLOBAL_SERVER_URL = env('GLOBAL_SERVER_URL')
 
 
-def basic_data_transform(df:pd.DataFrame) -> pd.DataFrame:
+def basic_data_transform(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-def long_data_transform(lock, dag:DAG, df:pd.DataFrame, collection_name:str, pipeline_id:str, pipeline:dict, experiment_number:int) -> pd.DataFrame:
+
+def long_data_transform(lock, dag: DAG, df: pd.DataFrame, collection_name: str, pipeline_id: str, pipeline: dict, experiment_number: int) -> pd.DataFrame:
     lock.acquire()
     """
     get all data node, compare collection_name
@@ -41,7 +42,8 @@ def long_data_transform(lock, dag:DAG, df:pd.DataFrame, collection_name:str, pip
     # if no exist data with same collection_name:
     if last_data_version == -1:
         new_node_id = "_".join([collection_name, '0'])
-        src_id = build_root_data_node(dag, df, collection_name, new_collection_version, pipeline_id, experiment_number, new_node_id=new_node_id)
+        src_id = build_root_data_node(dag, df, collection_name, new_collection_version,
+                                      pipeline_id, experiment_number, new_node_id=new_node_id)
 
     # if exist data with same collection_name, find data node, compare two dataset:
     else:
@@ -53,9 +55,10 @@ def long_data_transform(lock, dag:DAG, df:pd.DataFrame, collection_name:str, pip
             src_id = last_node_id
         # if not same: create data node with collection + surrogate_id
         else:
-            new_src_id = "_".join([collection_name, str(last_data_version + 1)]) 
-            src_id = build_child_data_node(dag, df, collection_name, new_collection_version, experiment_number, last_node_id, new_src_id)
-        
+            new_src_id = "_".join(
+                [collection_name, str(last_data_version + 1)])
+            src_id = build_child_data_node(
+                dag, df, collection_name, new_collection_version, experiment_number, last_node_id, new_src_id)
 
     # parse: pipeline dict to real pipline (chung)
     parsed_pipeline = parse(pipeline, 'collaborator')
@@ -64,12 +67,15 @@ def long_data_transform(lock, dag:DAG, df:pd.DataFrame, collection_name:str, pip
     pipe_param_string = parse_param(pipeline, 'collaborator')
     # print(pipe_param_string)
     for sub_pipeline in parsed_pipeline:
-        sub_pipeline_param_list = pipe_param_string[parsed_pipeline.index(sub_pipeline)]
+        sub_pipeline_param_list = pipe_param_string[parsed_pipeline.index(
+            sub_pipeline)]
 
         if(parsed_pipeline.index(sub_pipeline) == 0):
-            src_id = build_pipeline(dag, src_id, sub_pipeline, param_list = sub_pipeline_param_list, experiment_number=experiment_number)
+            src_id = build_pipeline(
+                dag, src_id, sub_pipeline, param_list=sub_pipeline_param_list, experiment_number=experiment_number)
         else:
-            src_id = build_pipeline(dag, src_id, sub_pipeline, param_list = sub_pipeline_param_list, experiment_number=experiment_number)
+            src_id = build_pipeline(
+                dag, src_id, sub_pipeline, param_list=sub_pipeline_param_list, experiment_number=experiment_number)
 
     print(dag.nodes_info)
 
@@ -78,26 +84,25 @@ def long_data_transform(lock, dag:DAG, df:pd.DataFrame, collection_name:str, pip
         print(i)
         time.sleep(1)
 
-    send_result_to_global_server(dag, pipeline_id, df, src_id)
+    send_result_to_global_server(dag, pipeline_id, src_id)
 
     lock.release()
 
-    return 
+    return
 
 
-def send_result_to_global_server(dag, pipeline_id, df, src_id):
+def send_result_to_global_server(dag, pipeline_id, src_id):
+    data_path = dag.get_node_attr(src_id)['filepath']
+    dataframe = pd.read_csv(data_path)
     # send pipeline_id, json dag, dataframe to global-server
     url = f'{GLOBAL_SERVER_URL}/pipeline/merge'
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     data = {
         "pipeline_id": pipeline_id,
         "dag_json": dag.get_dict_graph(),
-        "dataframe": pickle_encode(df),
+        "dataframe": pickle_encode(dataframe),
         "last_node": src_id
     }
 
-
-    
     r = requests.post(url, headers=headers, data=json.dumps(data))
     print("response: ", r.text)
-    
